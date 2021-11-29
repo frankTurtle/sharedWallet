77cf3c818263da3df834a2ebcde9ef3bca340952
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-28 20:28:58 -0500

    Added MoneySent/Received events and added renounced ownership function

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -5,16 +5,26 @@ pragma solidity ^0.8.1;
 import './Allowance.sol';
 
 contract SharedWallet is Allowance {
+    // ******** EVENTS ******** 
+    event MoneySent( address indexed _beneficiary, uint _amount );
+    event MoneyReceived( address indexed _from, uint _amount );
+
+    // ******** FUNCTIONS ******** 
     function withdrawMoney( address payable _to, uint _amount ) public ownerOrAllowed( _amount ) {
         require( _amount <= address(this).balance, "Contract doesn't own enough money");
 
         if(!isOwner()) { reduceAllowance(msg.sender, _amount); }
 
+        emit MoneySent( _to, _amount );
         _to.transfer(_amount);
+    }
+
+    function renounceOwnership() public override onlyOwner {
+        revert("No bueno duderino. You will ALWAYS own this contract!"); // not possible with this smart contract
     }
 
     // fallback function for when someone sends the contract monies but they fat-finger something
     receive() external payable {
-
+        emit MoneyReceived( msg.sender, msg.value );
     }
 }
