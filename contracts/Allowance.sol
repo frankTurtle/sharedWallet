c2eb65ad46472b8bad96b9bd61b12637fd1d2716
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-28 20:20:22 -0500

    fixed the modifier name - added the AllowanceChanged event!

--- contracts/Allowance.sol
+++ contracts/Allowance.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.1;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract Allowance is Ownable {
+    // ******** STATE VARIABLES ******** 
     mapping( address => uint ) public allowance;
+
+    // ******** EVENTS ******** 
+    event AllowanceChanged( address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount );
 
     // address public owner; // REMOVED TO USE OPENZEPPELIN CONTRACT INSTEAD
 
@@ -24,6 +28,7 @@ contract Allowance is Ownable {
      * 
      */
     function addAllowance( address _who, uint _amount ) public onlyOwner {
+        emit AllowanceChanged( _who, msg.sender, allowance[_who], _amount );
         allowance[_who] = _amount;
     }
 
@@ -34,7 +39,8 @@ contract Allowance is Ownable {
         return owner() == msg.sender;
     }
 
-    function reduceAllowance( address _who, uint _amount ) internal ownerOrAllowed( _amount ) {
+    function reduceAllowance( address _who, uint _amount ) internal isOwnerOrAllowed( _amount ) {
+        emit AllowanceChanged( _who, msg.sender, allowance[_who], allowance[_who] - _amount );
         allowance[_who] -= _amount;
     }
 }
